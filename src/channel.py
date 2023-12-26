341b2a148c71f6a76d2ea7ce38d1b341ab72ce1d
ZhenyaKasatkina <knopisha.zh@gmail.com>
2023-12-27 00:36:32 +0300

    change src/channel.py, add requirements.txt

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,23 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """
+        Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
+        """
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        """
+        Выводит в консоль информацию о канале.
+        """
+        dict_to_print = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
